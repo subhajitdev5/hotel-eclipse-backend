2049f0f4a4f58de93ffe26356e736d656adf4801
Subhajit Ghosh <subhajitgudp01@gmail.com>
2024-10-10 18:18:24 +0530

    All

---
+++ src/main/java/com/example/hotel/HotelEclipseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelEclipseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelEclipseBackendApplication.class, args);
+	}
+
+}
