76e34fe0315616962307926d51bb26e6fe798f40
Subhajit Ghosh <subhajitgudp01@gmail.com>
2024-10-15 01:04:08 +0530

    final backend

---
+++ src/main/java/com/example/hotel/repository/RoomRepository.java
@@ -0,0 +1,13 @@
+package com.example.hotel.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.hotel.model.Room;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+//	List<Room> findByAvailableTrue();
+}
